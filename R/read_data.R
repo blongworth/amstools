103ba5cdde768bdcfeabf0a6bdf7a2c7c095d4a3
Brett Longworth <blongworth@gmail.com>
2016-02-08 15:53:43 -0500

    Added functions for loading wheels from files.

--- R/read_data.R
+++ R/read_data.R
@@ -2,14 +2,44 @@
 #
 # Functions to load raw wheel data from file or database
 
-# read raw results data from file
-read_wheel <- function(wheel) {
-  print("Hello, world!")
+#' Read raw results data from file
+#' 
+#' @param file A file path to a SNICS format results file.
+#' @return A data frame of the wheel results.
+
+readResfile  <- function (file) {
+  read.delim(file, skip = 4, comment.char = "=")
+}
+
+#' Process wheel results data frame.
+#' 
+#' @param data A data table as returned by readResfile.
+#' @return A results data table with fixed and calculated fields.
+
+mungeResfile  <- function (z) {
+  z %>% mutate(ts = as.POSIXct(strptime(
+                  Run.Completion.Time, format = "%a %b %d %H:%M:%S %y")), 
+               Pos = as.factor(Pos),
+               ce = 1/sqrt(CntTotGT), #Add counting error
+               cor1412he = X14.12he/X13.12he^2 * 1E9, #Add corrected 14/12
+               X14.12he = X14.12he * 1E12, #Convert ratio to 1E12
+               he12C = he12C * 1E6, #Convert current to uA
+               le12C = le12C * 1E6)
+}
+
+# read a wheel file
+readWheelfile <- function(wheel) {
+  # won't work for CFAMS wheels
+  read.delim(file)
+  
 }
 
 # read raw results from database
 
 # read analysed wheel from file
+readSnicsfile <- function(wheel) {
+  print("Hello, world!")
+}
 
 # read analysed wheel from database
 
