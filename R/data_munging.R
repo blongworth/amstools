501f499de77cea82ab3a209dd2700d7076c9abe9
Brett Longworth <blongworth@gmail.com>
2016-02-10 17:44:30 -0500

    fixed a bunch of stuff, added roxygen docs

--- R/data_munging.R
+++ R/data_munging.R
@@ -1,12 +1,21 @@
 # Functions for cleaning up and calculations on data pulled from the database
 
+
+#' Modify and calculate fields for raw standards data.
+#'
+#' @param data A data frame as returned by getStandards
+#' @param std The standards table as returned by getStdTable
+#'
+#' @return A data frame with calculated fields.
+#' @export
+#' @importFrom magrittr %>%
 mungeStandards <- function(data, std) {
 
   #join standard table
-  data <- inner_join(data, std, by = "rec_num")
+  data <- dplyr::inner_join(data, std, by = "rec_num")
 
   out <- data %>%
-    mutate(rep_err = pmax(f_int_error, f_ext_error),
+    dplyr::mutate(rep_err = pmax(f_int_error, f_ext_error),
            normFm = normFm(f_modern, fm_exp),
            sigma = sigma(f_modern, fm_exp, rep_err),
            frep_err = rep_err/f_modern,
@@ -16,37 +25,35 @@ mungeStandards <- function(data, std) {
            le12c = ifelse(system == "USAMS", le12c * -1, le12c),
            #is ox-i primary?
            primary = (((sample_type == "S") | (sample_type_1 == "S")) & (grepl("OX-I", name)))) %>%
-    select(-target_name, -f_int_error, -f_ext_error, -sample_type,
+    dplyr::select(-target_name, -f_int_error, -f_ext_error, -sample_type,
            -sample_type_1) %>%
     #number of splits?
-    group_by(osg_num) %>% #For each osg_num
-    mutate(splits = n()) #Count occurrences to get number of splits
+    dplyr::group_by(osg_num) %>% #For each osg_num
+    dplyr::mutate(splits = n()) #Count occurrences to get number of splits
 
   return(out)
 }
 
 
-calcQC <- function(data) {
+#' Format QC Table to match standards table format
+#'
+#' @param data A data frame as returned by getQCStandards.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' @importFrom magrittr "%>%"
+formatQC <- function(data) {
   # add calculated fields to qc table data and rename to match other tables
 #Filter data
   out <- data %>%
-    mutate(rep_err = pmax(f_int_error, f_ext_error),
-           normFm = normFm(f_modern, fm_exp),
-           sigma = sigma(f_modern, fm_exp, rep_err),
-           frep_err = rep_err/f_modern,
-           #system
-           system = substring(wheel, 1, 5),
-           #fix CFAMS 12C
-           le12c = ifelse(system == "USAMS", le12c * -1, le12c),
-           #is ox-i primary?
-           primary = (((sample_type == "S") | (sample_type_1 == "S")) & (grepl("OX-I", name)))) %>%
-    select(-target_name, -f_int_error, -f_ext_error, -sample_type,
+    dplyr::select(-target_name, -f_int_error, -f_ext_error, -sample_type,
            -sample_type_1) %>%
     #number of splits?
-    group_by(osg_num) %>% #For each osg_num
-    mutate(splits = n()) #Count occurrences to get number of splits
+    dplyr::group_by(osg_num) %>% #For each osg_num
+      dplyr::mutate(splits = n()) #Count occurrences to get number of splits
 
-  return(out)
 sec <- qc %>%
   mutate(
     system = substring(wheel, 1, 5),
@@ -58,6 +65,16 @@ sec <- qc %>%
 }
 
 
+#' Get and format standards data
+#'
+#' @param from Character vector of date in form 'YYYY-MM-DD'.
+#' @param to Character vector of date in form 'YYYY-MM-DD'.
+#' @param sys Character vector of system name: 'cfams', 'usams', or 'both'.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 getQCData <- function(from, to = "present", sys = "both") {
   # Function to get standards from database and return munged table
 
--- R/general_functions.R
+++ R/general_functions.R
@@ -1,26 +1,76 @@
 # General data reduction functions
 
-#relative sd
+#' Relative SD
+#'
+#' @param x A numeric vector.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 rsd <- function(x) {
   sd(x)/mean(x)
 }
 
+
+#' Standard Error
+#'
+#' @param x A numeric vector.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 se <- function(x) {
-  #Calculate standard error
   sqrt(var(x)/length(x))
 }
 
+
+#' Sigma
+#'
+#' Also known as a z-score.
+#'
+#' @param fmm Measured value.
+#' @param fmc Expected value.
+#' @param em Measurement error.
+#' @param ec Error of expected value.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 sigma <- function(fmm, fmc, em, ec = 0) {
-  #Calculate Sigma
-  #also known as a z-score
   (fmm - fmc) / sqrt(em^2 + ec^2)
 }
 
+#' Normalized Value
+#'
+#' Normalize a value using the expected value or population mean.
+#'
+#' @param fmm Measured value.
+#' @param fmc Expected value.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 normFm <- function (fmm, fmc) {
   #Calculate normalized fm
   (fmm - fmc) / fmc
 }
 
+
+#' Intrinsic Error
+#'
+#' Calculate the "extra" (intrinsic, residual) error.
+#'
+#' @param popErr Standard deviation of the population.
+#' @param targErr Measurement error of a measurement.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 intrErr <- function(popErr, targErr) {
   #calculate intrinsic error
   if (targErr > totErr) {
@@ -30,6 +80,19 @@ intrErr <- function(popErr, targErr) {
   }
 }
 
+
+#' Total error.
+#'
+#' Calculate the predicted variability in a population given a measurement
+#' error and an intrisic error.
+#'
+#' @param targErr Measurement error.
+#' @param intrErr Intrinsic error.
+#'
+#' @return
+#' @export
+#'
+#' @examples
 totErr <- function(targErr, intrErr) {
 #Calculate total error
   sqrt(targErr^2 + intrErr^2)
