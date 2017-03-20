6131c85ca473fcf305a75b8f4da2e8b2b957cb04
Brett Longworth <blongworth@gmail.com>
2017-03-20 15:11:23 -0400

    add test

--- tests/testthat/test_db_access.R
+++ tests/testthat/test_db_access.R
@@ -4,4 +4,11 @@ test_that("conNOSAMS returns db connection", {
 	expect_equal(class(conNOSAMS()), "RODBC")
 })
 
+from <- '01-01-2017'
+to <- '01-10-2017'
+sys <- 'both'
+
+test_that("getStandards returns datatable", {
+	expect_true(is.data.frame(getStandards(from, to, sys, getcurrents = FALSE)))
+})
 RODBC::odbcCloseAll()
