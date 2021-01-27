68476eb864c1a1da00a5720ef011d66192e410e0
Brett Longworth <blongworth@whoi.edu>
2021-01-27 17:38:49 -0500

    Fix tests for updated blank correction method.

--- tests/testthat/test_data_reduction.R
+++ tests/testthat/test_data_reduction.R
@@ -1,13 +1,12 @@
 context("Data Reduction")
 
-data <- getWheel("CFAMS020620") %>%
-        select(wheel_pos, sample_name, norm_ratio, 
-                int_err, ext_err, fm_corr, sig_fm_corr,
-                lg_blk_fm, sig_lg_blk_fm, fm_mb_corr,
-                sig_fm_mb_corr, blank_fm, sig_blank_fm,
-                blank_mass, sig_blank_mass) %>%
-        mutate(max_err = pmax(int_err, ext_err))
-# need to join dc13 for mass of sample?
+data <- getWheel("CFAMS011421") %>%
+  select(wheel_pos, sample_name, norm_ratio,
+         int_err, ext_err, fm_corr, sig_fm_corr,
+         lg_blk_fm, sig_lg_blk_fm, fm_mb_corr, sig_fm_mb_corr,
+         blank_fm, sig_blank_fm, tot_mass, sig_tot,
+         blank_mass, sig_blank_mass) %>%
+  mutate(max_err = pmax(int_err, ext_err))
 
 context("Large Blank Correction")
 
@@ -15,34 +14,47 @@ test_that("LBC returns correct LBC Fm", {
         expect_equal(doLBC(1, 0, 1), 1)
         expect_equal(doLBC(0, 0, 1), 0)
         # ceylon should be no correction, but snics_results shows different cor val
-        expect_equal(doLBC(data[1,3], data[1,8], 1.0398), data[1,3]) 
-        # C-8 OC true value is different at 0.000001 level
-        expect_equal(doLBC(data[5,3], data[5,8], 1.0398), data[5,6]) 
-        # Small C-2 (lgblk from database)
-        expect_equal(doLBC(data[7,3], data[7,8], 1.0398), data[7,6]) 
+        expect_equal(doLBC(data[1,3], data[1,8], 1.0398), data[1,3])
+        # C-2 true value is different at 0.000001 level
+        expect_equal(doLBC(data[5,3], data[5,8], 1.0398), data[5,6])
+        # TIRI-I
+        expect_equal(doLBC(data[6,3], data[6,8], 1.0398), data[6,6])
 
 })
 
 test_that("LBC returns correct LBC Fm error", {
-  expect_equal(doLBCerr(1, 0, 1, 0, 0), 0) # sqrt 0
-        # ceylon should be no correction, but snics_results shows different cor val
-        expect_equal(doLBCerr(data[1,3], data[1,8], 1.0398, data[1,-1], data[1,9]), data[1,7]) 
-        # C-8 OC true value is different at 0.000001 level
-        expect_equal(doLBCerr(data[5,3], data[5,8], 1.0398, data[5,-1], data[5,9]), data[5,7]) 
-        # Small C-2 (lgblk from database)
-        expect_equal(doLBCerr(data[7,3], data[7,8], 1.0398, data[7,-1], data[7,9]), data[7,7]) 
+  expect_equal(doLBCerr(fmmeas = 1, fmblank = 0, fmstd = 1, fmmeaserr = 0, fmblankerr = 0), 0) # sqrt 0
+  # ceylon should be no correction, but snics_results shows different cor val
+  expect_equal(doLBCerr(data[1,3], data[1,8], 1.0398, data[1,18], data[1,9]), data[1,7])
+  # C-2
+  expect_equal(doLBCerr(data[5,3], data[5,8], 1.0398, data[5,18], data[5,9]), data[5,7])
+  # TIRI-I
+  expect_equal(doLBCerr(data[6,3], data[6,8], 1.0398, data[6,18], data[6,9]), data[6,7])
 })
 
 context("Mass Balance Correction")
 
+test_that("doMBC checks for valid input", {
+  expect_error(doMBC("blue", 0, 0, 1))
+  expect_error(doMBC(-1, 0, 0, 1))
+  expect_error(doMBC(1, 0, 0, 0))
+  expect_error(doMBC(0, 0, 1, 1))
+})
+
 test_that("MBC returns correct MBC Fm", {
-  expect_equal(doMBC(1, 0, 1, 1), 2)
-  expect_equal(doMBC(0, 0, 1, 1), 0)
-        expect_equal(doMBC(data[1,6], data[1,12], data[1,14], data[1,9]), data[1,7]) 
-  
+  expect_equal(doMBC(fmmeas = 1, fmblank = 1, massmeas = 1, massblank = 0), 1)
+  expect_equal(doMBC(1, 0, 2, 1), 2)
+  expect_equal(doMBC(data[5,6], data[5,12], data[5,14], data[5,16]), data[5,10])
+  expect_equal(doMBC(data[6,6], data[6,12], data[6,14], data[6,16]), data[6,10])
 })
 
 test_that("MBC returns correct MBC Fm Err", {
-  expect_equal(doMBCerr(1, 0, 1, 1, 0, 0, 0, 0), 0) # sqrt 0
+  expect_equal(doMBCerr(1, 0, 1, 0, 0, 0, 0, 0), 0) # sqrt 0
+  expect_equal(doMBCerr(data[5,6], data[5,12], data[5,14], data[5,16],
+                     data[5,7], data[5,13], data[5,15], data[5,17]),
+               data[5,11])
+  expect_equal(doMBCerr(data[6,6], data[6,12], data[6,14], data[6,16],
+                     data[6,7], data[6,13], data[6,15], data[6,17]),
+               data[6,11])
 })
 
